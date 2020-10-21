b94945a2539ac517030a45358562c2e2a9ac992b
trangtrangbk <tranginspirit78@gmail.com>
2020-10-21 17:21:16 +0700

    update get room

--- controllers/hotels/getHotelById.js
+++ controllers/hotels/getHotelById.js
@@ -1,12 +1,14 @@
 const { getHotel } = require("../../services/hotelService");
-
+const {getRooms} = require("../../services/roomService")
 const { InternalServerError } = require("../../utils/ResponseHelper");
 
 const get = async (req, res) => {
   try {
     const {id} = req.params
     let arr = await getHotel({_id : id});
-    res.send(arr);
+    const rooms = await getRooms({hotelId : arr._id})
+    console.log({...arr});
+    res.send({...arr._doc, rooms});
   } catch (e) {
     console.log(e);
     InternalServerError(res);
